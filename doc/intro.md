18a0f9b691276f16efa5d48f21171ec030061882
Mårten Larsson <mala235813@gmail.com>
2019-04-05 21:52:55 +0200

    Add a new lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dist-db
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dist_db/core_test.clj
@@ -0,0 +1,7 @@
+(ns dist-db.core-test
+  (:require [clojure.test :refer :all]
+            [dist-db.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
