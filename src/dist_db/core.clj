acf6127efa2cfa6d012417081629c0ab47da9aeb
Mårten Larsson <mala235813@gmail.com>
2019-04-07 22:08:03 +0200

    Error handling

--- src/dist_db/core.clj
+++ src/dist_db/core.clj
@@ -9,6 +9,9 @@
 (defn- java-path [path]
   (java.nio.file.Paths/get path (into-array String [""])))
 
+(defn- java-path-name [java-path]
+  (.getFileName java-path))
+
 (defn- java-parent-path [java-path]
   (.getParent java-path))
 
@@ -17,6 +20,9 @@
 
 ;; Specs
 ;; ------------------------
+(defn is-java-path? [obj]
+  (instance? java.nio.file.Path obj))
+
 (defn path-exists? [path]
  (.exists (io/file path)))
 
@@ -30,25 +36,40 @@
  (.canWrite (io/file path)))
 
 (s/def
-  ::make-db-args
+  ::make-db-args-1
+  #(not (path-exists? %)))
+
+(s/def
+  ::make-db-args-2
   (s/cat :path (s/and path-exists?
                       is-directory?
                       can-write?)
-         :db-name not-path-exists?))
+         :db-name  string? ))
 
 (s/def
 	::make-db-ret
-	(s/keys :req [::path]))
+	(s/keys :req [::db-path ::db-name]))
 
 (s/fdef make-db
   :args ::make-db-args
   :ret ::make-db-ret)
 
+(defn s-valid-or-throw [spec & args]
+  (let [data (if (= 1 (count args)) (first args) args)]
+    (cond (not (s/valid? spec data))
+          (throw (AssertionError. (s/explain-str spec data))))))
+
 ;; Database API|
 ;; ------------------------
 (defn make-db
   "Returns a db defnition"
-  [path db-name]
-  (s/explain ::make-db-args [path db-name])
-  {:path path
-   :db-name db-name})
+  ([path]
+   (s-valid-or-throw ::make-db-args-1 path)
+   (let [j-path (java-path path)
+         parent-path (str (java-parent-path j-path))
+         db-name (str (java-path-name j-path))]
+     (make-db parent-path db-name)))
+  ([path db-name]
+   (s-valid-or-throw ::make-db-args-2 [path db-name])
+   {:db-path path
+    :db-name db-name}))
