6bab1b7f1a6b8e45d252fd15f5ae74f7619b8fd5
Mårten Larsson <mala235813@gmail.com>
2019-04-07 18:40:25 +0200

    Add some specs

--- src/dist_db/core.clj
+++ src/dist_db/core.clj
@@ -1,6 +1,54 @@
-(ns dist-db.core)
+(ns dist-db.core
+ (:require [clojure.java.io :as io])
+ (:require [clojure.spec.alpha :as s])
+ (:require [clojure.spec.gen.alpha :as sg])
+ (:require [clojure.spec.test.alpha :as st]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Private helper functions
+;; ------------------------
+(defn- java-path [path]
+  (java.nio.file.Paths/get path (into-array String [""])))
+
+(defn- java-parent-path [java-path]
+  (.getParent java-path))
+
+(defn- java-combine-paths [java-path & java-paths]
+  (reduce #(.resolve %1 %2) java-path java-paths))
+
+;; Specs
+;; ------------------------
+(defn path-exists? [path]
+ (.exists (io/file path)))
+
+(defn not-path-exists? [path]
+  (not (path-exists? path)))
+
+(defn is-directory? [path]
+  (.isDirectory (io/file path)))
+
+(defn can-write? [path]
+ (.canWrite (io/file path)))
+
+(s/def
+  ::make-db-args
+  (s/cat :path (s/and path-exists?
+                      is-directory?
+                      can-write?)
+         :db-name not-path-exists?))
+
+(s/def
+	::make-db-ret
+	(s/keys :req [::path]))
+
+(s/fdef make-db
+  :args ::make-db-args
+  :ret ::make-db-ret)
+
+;; Database API|
+;; ------------------------
+(defn make-db
+  "Returns a db defnition"
+  [path db-name]
+  (s/explain ::make-db-args [path db-name])
+  {:path path
+   :db-name db-name})
